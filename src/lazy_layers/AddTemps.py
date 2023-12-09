d4af6688075156dc4bdede788dbe8ec40f02502f
SeanKozina <120601630+SeanKozina@users.noreply.github.com>
2023-12-09 13:53:33 -0800

    Update AddTemps.py

--- src/lazy_layers/AddTemps.py
+++ src/lazy_layers/AddTemps.py
@@ -13,4 +13,15 @@ class AddTemps(Layer):
         """Constructs a new AddIsland Layer Object"""
     
     def run(self, board: np.ndarray, rng: np.random.Generator) -> np.ndarray:
-        raise NotImplementedError
+        for i in range(board.shape[0]):
+            for j in range(board.shape[1]):
+                # Generate a random number to determine the temperature
+                temp = rng.integers(1, 7)  # 1-6 range, where 1-4 are warm, 5 is cold, 6 is freezing
+                if temp <= 4:
+                    board[i, j] = Cell.WARM
+                elif temp == 5:
+                    board[i, j] = Cell.COLD
+                else:
+                    board[i, j] = Cell.FREEZING
+        
+        return board
