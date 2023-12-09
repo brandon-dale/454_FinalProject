b8e365e1a00aa0065536672cd5ed5b0c0d137d2a
SeanKozina <120601630+SeanKozina@users.noreply.github.com>
2023-12-09 14:32:21 -0800

    Update Shore.py

--- src/lazy_layers/Shore.py
+++ src/lazy_layers/Shore.py
@@ -1,4 +1,5 @@
 import numpy as np
+form core import is_edge_cell
 from lazy_layers.layer import Layer
 
 class Shore(Layer):
@@ -7,6 +8,8 @@ class Shore(Layer):
     Add shore on edge of land and ocean.
     """
     DEPTH = 3
+    SWAMP_DEPTH = 5  # Increased depth for swamp shores, adjust as needed
+
     
     def __init__(self):
         """Constructs a new Shore Layer Object"""
@@ -17,4 +20,14 @@ class Shore(Layer):
         :param board: A 2D array containing the input board state
         :return: A new copy of the board after the transformation
         """
-        raise NotImplementedError
+	# Iterate over each cell in the board
+        for x in range(board.shape[0]):
+            for y in range(board.shape[1]):
+                # Check if the cell is at the edge
+                if is_edge_cell(board, x, y):
+                    # Determine if the cell is a swamp shore or a regular shore
+                    if board[x,y] == Cell.SWAMP:
+                        board[x, y] = Cell.SWAMP_SHORE
+                    else:
+                        board[x, y] = Cell.SHORE
+        return board
